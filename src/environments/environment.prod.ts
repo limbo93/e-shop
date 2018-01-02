4ea887fb6f1ead3a7bc9e7be6d0f5831ba03f2e1
limbo93 <mhriad0@gmail.com>
2018-01-02 16:56:11 +0600

    firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcQp50iNHevlTjYiKF_2d9tMrVn-ccz0s",
+    authDomain: "e-shop-5dbc3.firebaseapp.com",
+    databaseURL: "https://e-shop-5dbc3.firebaseio.com",
+    projectId: "e-shop-5dbc3",
+    storageBucket: "e-shop-5dbc3.appspot.com",
+    messagingSenderId: "860135354129"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCcQp50iNHevlTjYiKF_2d9tMrVn-ccz0s",
+    authDomain: "e-shop-5dbc3.firebaseapp.com",
+    databaseURL: "https://e-shop-5dbc3.firebaseio.com",
+    projectId: "e-shop-5dbc3",
+    storageBucket: "e-shop-5dbc3.appspot.com",
+    messagingSenderId: "860135354129"
+  }
 };
