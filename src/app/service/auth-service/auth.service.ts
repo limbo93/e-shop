bfc574751b16a31f8512880217a41be01960f82c
limbo93 <mhriad0@gmail.com>
2018-01-07 13:16:48 +0600

    bug fixed

--- src/app/service/auth-service/auth.service.ts
+++ src/app/service/auth-service/auth.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 import { AppUser } from '../user/domain/app.user.model';
 import { UserService } from '../user/user.service';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 
 @Injectable()
@@ -31,6 +32,10 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-      .switchMap(user => this.userService.get(user.uid))
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+
+        return Observable.of(null);
+      })
   }
 }
